3f41c10cba3031d98fb70b352c0d0276128dce15
MHDabdulkader <godholiak12345@gmail.com>
2024-10-12 20:18:21 +0600

    Home header, total amount (chart dependencies installed  works remains)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,33 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+
 import React from 'react'
 
 function Home() {
+  const loggedIn = {firstName: "Mr. Xyx"}
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type = "greeting"
+            title = "Welcome"
+            user = {loggedIn?.firstName || 'Guest'}
+            subtext = "Access and managed your account and transactions"
+          />
+
+          {/* total amount box */}
+          <TotalBalanceBox
+            accounts = {[]}
+            totalBanks = {1}
+            totalCurrentBalance = {1250.25}
+          />
+        </header>
+
+
+      </div>
+    </section>
   )
 }
 
